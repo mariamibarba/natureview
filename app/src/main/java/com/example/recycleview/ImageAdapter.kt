dd762e51603dd6eb669ef4301f191387329f12d4
marii <mariami.barbakadze.2@btu.edu.ge>
2021-01-27 21:01:26 +0400

    first commit

--- app/src/main/java/com/example/recycleview/ImageAdapter.kt
+++ app/src/main/java/com/example/recycleview/ImageAdapter.kt
@@ -10,26 +10,39 @@ import com.bumptech.glide.Glide
 
 class ImageAdapter(private val images: List<Image>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
-    class ImageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class ImageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    {
 
 
         val ImageView: ImageView = itemView.findViewById(R.id.setImage)
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder
+
+
+
+    {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.image_image, parent, false)
 
         return ImageViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
-        val p = images[position]
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int)
+
+    {
+        val k = images[position]
 
         Glide.with(holder.ImageView.context)
-                .load(p.url)
+
+                .load(k.url)
+
                 .centerCrop()
+
+
                 .into(holder.ImageView)
+
     }
 
     override fun getItemCount(): Int = images.size
--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.recycleview
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+
+
+
 import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
+
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -12,36 +17,39 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.activity_main)
+
+
+
 
         recyclerView = findViewById(R.id.recyclerView)
 
-        val images = ArrayList<Image>()
 
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_front.JPG"))
-        images.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG/1200px-Mercedes_C-Klasse_%28W203%29_Elegance_20090830_rear.JPG"))
+        val photos = ArrayList<Image>()
+
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg/1024px-Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg"))
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Tarvasj%C3%B5gi.jpg/1280px-Tarvasj%C3%B5gi.jpg"))
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg/1024px-Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg"))
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Tarvasj%C3%B5gi.jpg/1280px-Tarvasj%C3%B5gi.jpg"))
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg/1024px-Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg"))
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Tarvasj%C3%B5gi.jpg/1280px-Tarvasj%C3%B5gi.jpg"))
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg/1024px-Waaierbuisjeszwam_%28Polyporus_varius%29_op_een_dode_lijsterbes_%28Sorbus%29._Locatie._Natuurterrein_De_Famberhorst._08-07-2019._%28d.j.b%29._03-05.jpg"))
+        photos.add(Image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Tarvasj%C3%B5gi.jpg/1280px-Tarvasj%C3%B5gi.jpg"))
 
 
+        val adapter = ImageAdapter(photos)
 
 
-        val adapter = ImageAdapter(images)
+        recyclerView.adapter = adapter
+
+
 
         recyclerView.layoutManager = GridLayoutManager(this, 2)
-        recyclerView.adapter = adapter
 
     }
 }
